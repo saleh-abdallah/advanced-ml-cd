aee7d4f09c0f8ede49adc812ab63f9a78813a377
saleh-abdallah <94493974+saleh-abdallah@users.noreply.github.com>
2025-11-30 19:56:04 -0500

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 def test_prediction(client):
     response = client.post("/predict", json={"text": "I love this!"})
     assert response.status_code == 200
-=======
-def test_prediction(client):
-    response = client.post("/predict", json={"text": "I love this!"})
-    assert response.status_code == 200
->>>>>>> 1a1f71ae28618944827e6543f0f861c593d8a044
+
