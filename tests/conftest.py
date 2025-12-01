5a1865136cef0d267bb7aed50a31005fa8adeea5
saleh-abdallah <94493974+saleh-abdallah@users.noreply.github.com>
2025-11-30 20:02:19 -0500

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,13 @@
 # tests/conftest.py
+import sys
+import os
 import pytest
 from fastapi.testclient import TestClient
-from main import app  # make sure this is your FastAPI app
+
+# Add root folder to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from main import app
 
 @pytest.fixture
 def client():
